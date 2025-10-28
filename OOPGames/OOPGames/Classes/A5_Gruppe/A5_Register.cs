8358c673b969f4d6abcad065f1c1d6d14bd4ff2c
Niklas Gomeringer <niklas.gomeringer@gmail.com>
2025-10-28 11:59:19 +0100

    Niklas Gomeringer

--- OOPGames/OOPGames/Classes/A5_Gruppe/A5_Register.cs
+++ OOPGames/OOPGames/Classes/A5_Gruppe/A5_Register.cs
@@ -14,11 +14,15 @@ namespace OOPGames
             A5_Paint painter = new A5_Paint();
             A5_Rules rules = new A5_Rules();
             A5_Player player = new A5_Player();
+            A5_ComputerExtreme computerExtremePlayer = new A5_ComputerExtreme();
+            A5_ComputerEasy computerEasyPlayer = new A5_ComputerEasy();
 
             // Registriere beim OOPGamesManager
             OOPGamesManager.Singleton.RegisterPainter(painter);
             OOPGamesManager.Singleton.RegisterRules(rules);
             OOPGamesManager.Singleton.RegisterPlayer(player);
+            OOPGamesManager.Singleton.RegisterPlayer(computerExtremePlayer);
+            OOPGamesManager.Singleton.RegisterPlayer(computerEasyPlayer);
         }
     }
 }
