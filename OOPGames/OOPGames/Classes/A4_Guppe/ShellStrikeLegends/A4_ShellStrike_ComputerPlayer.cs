926989745643501fad292a1f0b49373501c1582b
Xenos321s <technick.2015.se@gmail.com>
2025-11-25 11:21:19 +0100

    Generative Terrain implementation und basic funktions. Refreshrate etc

--- OOPGames/OOPGames/Classes/A4_Guppe/ShellStrikeLegends/A4_ShellStrike_ComputerPlayer.cs
+++ OOPGames/OOPGames/Classes/A4_Guppe/ShellStrikeLegends/A4_ShellStrike_ComputerPlayer.cs
@@ -4,31 +4,27 @@ namespace OOPGames
 {
     public class A4_ShellStrike_ComputerPlayer : IComputerGamePlayer
     {
-        public string Name => "A4 ShellStrikeLegends Computer";
-
+        public string Name => "A4 ShellStrike AI";
         public int PlayerNumber { get; private set; }
+        private int _step = 0;
+        private Random _rnd = new Random();
 
-        public void SetPlayerNumber(int playerNumber)
-        {
-            PlayerNumber = playerNumber;
-        }
-
-        public bool CanBeRuledBy(IGameRules rules)
-        {
-            return rules is A4_ShellStrike_Rules;
-        }
-
-        public IGamePlayer Clone()
-        {
-            var c = new A4_ShellStrike_ComputerPlayer();
-            c.SetPlayerNumber(PlayerNumber);
-            return c;
-        }
+        public void SetPlayerNumber(int playerNumber) => PlayerNumber = playerNumber;
+        public bool CanBeRuledBy(IGameRules rules) => rules is A4_ShellStrike_Rules;
+        public IGamePlayer Clone() { var c = new A4_ShellStrike_ComputerPlayer(); c.SetPlayerNumber(PlayerNumber); return c; }
 
         public IPlayMove GetMove(IGameField field)
         {
-            // No moves in static preview mode
-            return null;
+            if (field is not A4_ShellStrike_Field shell) return null;
+            _step++;
+            // Simple behavior: move oscillating, adjust turret gradually, fire every 30 ticks
+            if (_step % 50 < 25)
+                return new A4_ShellStrike_Move(PlayerNumber, PlayerNumber==1?ShellStrikeAction.MoveRight:ShellStrikeAction.MoveLeft, 5);
+            if (_step % 50 < 40)
+                return new A4_ShellStrike_Move(PlayerNumber, ShellStrikeAction.TurretUp, 2);
+            if (_step % 50 < 49)
+                return new A4_ShellStrike_Move(PlayerNumber, ShellStrikeAction.TurretDown, 2);
+            return new A4_ShellStrike_Move(PlayerNumber, ShellStrikeAction.Fire, 0);
         }
     }
 }
